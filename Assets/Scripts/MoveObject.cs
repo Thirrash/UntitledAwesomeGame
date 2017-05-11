93754369565f088e908a477c8ecde2d8e4d55d6d
Thirrash <thirrasch@gmail.com>
2017-05-11 18:18:55 +0200

    Introduction to Inverse Kinematics and basic Player model

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AwesomeGame.PlayerMgmt
+{
+    public class MoveObject : MonoBehaviour
+    {
+        float timer = 0.0f;
+        public float TimeToMove = 1.0f;
+        Vector3 startLocalPos;
+        public Vector3 endLocalPos;
+
+        void Start( ) {
+            startLocalPos = transform.localPosition;
+            StartCoroutine( Move( ) );
+        }
+
+        void Update( ) {
+            timer += Time.deltaTime;
+        }
+
+        IEnumerator Move( ) {
+            while( true ) {
+                if( timer >= TimeToMove ) {
+                    Vector3 tmp = endLocalPos;
+                    endLocalPos = startLocalPos;
+                    startLocalPos = tmp;
+                    timer = 0.0f;
+                }
+
+                transform.localPosition = Vector3.Lerp(startLocalPos, endLocalPos, timer / TimeToMove);
+                yield return null;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/SoundHandler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace AwesomeGame.SoundMgmt
+{
+    public class SoundHandler : MonoBehaviour
+    {
+        public Dictionary<SoundType, AudioClip> Sounds { get; private set; }
+
+        [SerializeField]
+        List<SoundType> soundTypes = new List<SoundType>( );
+
+        [SerializeField]
+        List<AudioClip> soundClips = new List<AudioClip>( );
+
+        AudioSource audioSource;
+
+        void Start( ) {
+            audioSource = GetComponent<AudioSource>( );
+            Sounds = new Dictionary<SoundType, AudioClip>( );
+            if( soundTypes.Count != soundClips.Count )
+                Debug.LogError( "SoundTypes count does not equal SoundClips count in gameObject = " + gameObject.name );
+
+            for( int i = 0; i < soundTypes.Count; i++ )
+                Sounds.Add( soundTypes[i], soundClips[i] );
+        }
+
+        public bool PlayClip( AnimationEvent e ) {
+            if( !Sounds.ContainsKey( (SoundType)Enum.Parse( typeof( SoundType ), e.stringParameter ) ) )
+                return false;
+
+            audioSource.PlayOneShot( Sounds[(SoundType)Enum.Parse( typeof( SoundType ), e.stringParameter )] );
+            return true;
+        }
+
+        public bool PlayClip( SoundType type ) {
+            if( !Sounds.ContainsKey( type ) )
+                return false;
+
+            audioSource.PlayOneShot( Sounds[type] );
+            return true;
+        }
+    }
+}
+
